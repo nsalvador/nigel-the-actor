2a637737931b86eeac4cb14df22b353d49ac9c0c
nsalvador <nigelsalvador@gmail.com>
2020-03-09 01:09:04 -0400

    Added a possible solution for SPA on Github Pages

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,11 @@ export default {
   name: "App",
   mounted() {
     document.documentElement.style.overflow = "auto";
+    const path = localStorage.getItem("path");
+    if (path) {
+      localStorage.removeItem("path");
+      this.$router.push({ path });
+    }
   },
   components: {
     AppHeader: () => import("./components/core/Header.vue"),
